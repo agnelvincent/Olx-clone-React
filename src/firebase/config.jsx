64b5693041e852090d24e5e6850b7157044c0d3c
agnel <agnelvincent779@gmail.com>
2025-05-25 14:46:28 +0530

    added configuration of firebase

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -13,4 +13,14 @@ const firebaseConfig = {
   measurementId: "G-5SQ5B158F0"
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB-X6YQpfy28aR5wdzXYEkgNTK3oYN4p3E",
+//   authDomain: "olxclone-fc145.firebaseapp.com",
+//   projectId: "olxclone-fc145",
+//   storageBucket: "olxclone-fc145.firebasestorage.app",
+//   messagingSenderId: "42469907606",
+//   appId: "1:42469907606:web:e3ffe31d453c2175d7f6b7",
+//   measurementId: "G-5ZYBHCL2DZ"
+// };
+
   export default firebase.initializeApp(firebaseConfig);
