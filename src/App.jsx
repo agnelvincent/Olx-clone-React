8182ba161c0fedfc9f1fcad4a36421f1daff59ca
agnel <agnelvincent779@gmail.com>
2025-05-25 21:08:09 +0530

    final one

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React, { useContext, useEffect } from "react";
-
 import Home from "./Pages/Home";
-
 import Signup from "./Pages/SignUp";
-
 import Login from "./Pages/Login"
-
 import Create from "./Pages/Create"
-
 import ViewPost from "./Pages/ViewPost"
-
 import { AuthContext } from "./store/Context";
-
 import { FirebaseContext } from "./store/Context"; 
-
 import Post from "./store/PostContext";
-
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
-
 import './App.css'
+
 
 function App(){
 
@@ -30,7 +21,7 @@ function App(){
     firebase.auth().onAuthStateChanged((user)=>{
       setUser(user)
     })
-  })
+  },[]);
 
   return(
     <>
